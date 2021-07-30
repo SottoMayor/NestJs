489e8ec65f03064c9cee3f51558ee8f5615e0c1b
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-30 11:53:14 -0400

    Handling Errors, that involves find todo by ID

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './todos.model';
 import { TodoStatus } from './todos.model';
 import { v4 as uuid } from 'uuid'
@@ -53,16 +53,30 @@ export class TodosService {
   }
 
   public getTodoById(id: string): Todo{
-    return this.todos.find( todoItem => todoItem.id === id)
+    const todoFound = this.todos.find( todoItem => todoItem.id === id);
+    if(!todoFound){
+      throw new NotFoundException(`The todo with ID ${id} was not found!`)
+    }
+    return todoFound;
   }
 
   public deleteTodoById(id: string): void{
     const deletedTodoIndex = this.todos.findIndex(todoItem => todoItem.id === id);
+
+    if(deletedTodoIndex === -1){
+      throw new NotFoundException(`The Todo with ID ${id} was not found!`);
+    }
+
     this.todos.splice(deletedTodoIndex, 1);
   }
 
   public updatedTodoById(id: string, status: string): Todo{
     const updatedTodoIndex = this.todos.findIndex( todoItem => todoItem.id === id );
+
+    if(updatedTodoIndex === -1){
+      throw new NotFoundException(`The Todo with ID ${id} was not found!`);
+    }
+
     this.todos[updatedTodoIndex] = {
       ...this.todos[updatedTodoIndex],
       status: TodoStatus[status]
