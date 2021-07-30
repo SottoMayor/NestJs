38f5be4b087e82473795ff033dedebb469e2d87f
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-30 08:26:36 -0400

    Getting All Todos and working with mocked data

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
-    constructor(private todosServices: TodosService) {}
+  constructor(private todosServices: TodosService) {}
+
+  @Get()
+  getAllTodos() {
+    return this.todosServices.getAllTodos();
+  }
 }
