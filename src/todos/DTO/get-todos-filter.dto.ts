6df461ed1a464b6b7c0637474c186cd2224c529f
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-30 12:38:13 -0400

    Validating the getTodosFilter Method

--- src/todos/DTO/get-todos-filter.dto.ts
+++ src/todos/DTO/get-todos-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TodoStatus } from "../todos.model";
+import { IsOptional, IsString, IsEnum } from "class-validator";
 
 export class GetTodosFilterDto{
+    @IsOptional()
+    @IsEnum(TodoStatus)
     status?: TodoStatus;
+
+    @IsOptional()
+    @IsString()
     search?: string;
 }
