4260c5b4444e013d5f16eb88d1cabb1e7c41fc89
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-30 15:39:05 -0400

    Setting up the datebase connection e installing TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'kesavan.db.elephantsql.com',
+      port: 5432,
+      username: 'axcdeupn',
+      password: 'L21Ukz076jBggfV4eaY18xEQj-QkjPxv',
+      database: 'axcdeupn',
+      autoLoadEntities: true,
+      synchronize: true
+    }),],
 })
 export class AppModule {}
