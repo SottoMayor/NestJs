e05a5862b15ce1f56a2847aca0da36c65f4075c1
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-30 11:36:19 -0400

    Working with Validation, validating the creation of Todos

--- src/todos/DTO/create-todos.dto.ts
+++ src/todos/DTO/create-todos.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, Length } from "class-validator";
+
 export class CreateTodoDto {
+    @Length(5, 100, {
+        message: "This message was provided by me, the developer!"
+    })
     title: string;
+
+
+    @IsNotEmpty()
     text: string;
 }
