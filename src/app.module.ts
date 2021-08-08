7bc80797f26f8e1bbe41cce8004fe8ab4f384f46
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-08 11:23:21 -0400

    Setting up Auth configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TodosModule, 
@@ -8,11 +9,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       host: 'kesavan.db.elephantsql.com',
       port: 5432,
-      username: 'axcdeupn',
-      password: 'L21Ukz076jBggfV4eaY18xEQj-QkjPxv',
-      database: 'axcdeupn',
+      username: 'slljizru',
+      password: 'QiidfZKDr91wUkSp4dMxxa822DcZxYWm',
+      database: 'slljizru',
       autoLoadEntities: true,
       synchronize: true
-    }),],
+    }), AuthModule,],
 })
 export class AppModule {}
