8abb480d28e411dde612a13bacc09eac87850745
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-08 12:13:58 -0400

    Validation a user's credentials

--- src/auth/DTO/auth-credentials.dto.ts
+++ src/auth/DTO/auth-credentials.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Length, Matches } from "class-validator";
+
 export class authCredentialsDto{
+    @IsString()
+    @Length(4, 50, { message: 'The username must be length beetwen 4 and 50 characters.' })
     username: string;
+
+    @IsString()
+    @Length(8, 255, { message: 'The password must be at least 8 characters.' })
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { 
+        message: 'The password must contain at least 1 upper case letter, 1 lower case letter, 1 number or special character.' 
+    })
     password: string;
 }
